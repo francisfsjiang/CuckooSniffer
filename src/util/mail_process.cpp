24a1f0461ba2ff9b881736b1bece84a78d8745ab
Neveralso <neveralso@gmail.com>
2016-07-27 16:40:17 +0800

    remove strstream

--- src/util/mail_process.cpp
+++ src/util/mail_process.cpp
@@ -1,8 +1,8 @@
 #include "util/mail_process.hpp"
 
 #include <vector>
-#include <strstream>
 #include <iostream>
+#include <sstream>
 #include <map>
 #include <regex>
 #include <set>
@@ -27,7 +27,7 @@ bool is_boundary(const std::string &line, const std::string &boundary) {
 
 std::vector<File *> mail_process(const std::string &data) {
 
-    std::istrstream input_stream(data.c_str());
+    std::istringstream input_stream(data.c_str());
 
     std::multimap<std::string, std::string> record;
 
